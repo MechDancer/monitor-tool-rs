1b157e4bf08c70fe7b185cbbb93d7f0ece0e334f
YdrMaster <ydrml@hotmail.com>
2021-09-20 08:36:33 +0800

    feat: 实现解码

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 pub mod canvas2d;
 pub mod protocol;
 
+#[derive(Clone, Copy)]
 pub struct Pose {
     pub x: f32,
     pub y: f32,
--- src/protocol.rs
+++ src/protocol.rs
@@ -1,11 +1,15 @@
 ﻿use super::Pose;
+use iced::Color;
 
 pub struct FrameInputStream<'a> {
     title: &'a str,
     buffer: &'a [u8],
 }
 
-pub struct PoseInputStream<'a>(&'a [u8]);
+pub struct PoseInputStream<'a> {
+    state: Color,
+    buffer: &'a [u8],
+}
 
 pub struct Frame<'a> {
     title: &'a str,
@@ -17,7 +21,14 @@ pub struct Frame<'a> {
     poses: PoseInputStream<'a>,
 }
 
+#[derive(Clone, Copy)]
 struct RGBA(u8, u8, u8, u8);
+
+#[derive(Clone, Copy)]
+union PoseOrElse {
+    pose: Pose,
+    color: (f32, RGBA),
+}
 
 #[repr(C)]
 struct Config {
@@ -26,24 +37,46 @@ struct Config {
     pose: Pose,
 }
 
+macro_rules! sizeof {
+    ($t:ty) => {
+        std::mem::size_of::<$t>()
+    };
+}
+
 impl<'a> FrameInputStream<'a> {
-    fn from(buffer: &'a [u8]) -> Self {
+    /// 从字节数组构造一个帧流
+    fn new(buffer: &'a [u8]) -> Self {
         let mut result = Self { title: "", buffer };
         result.title = result.slice_str();
         result
     }
 
+    /// 从流前分割一个字符串
     fn slice_str(&mut self) -> &'a str {
         let len = self.buffer[0] as usize;
-        let str = unsafe { std::str::from_utf8_unchecked(&self.buffer[1..1 + len]) };
-        self.buffer = &self.buffer[1 + len..];
-        str
+        let end = 1 + len;
+        let slice = &self.buffer[1..end];
+        self.buffer = &self.buffer[end..];
+        unsafe { std::str::from_utf8_unchecked(slice) }
     }
 
+    /// 从流前分割一个位姿流
+    fn slice_poses(&mut self) -> PoseInputStream<'a> {
+        let len = unsafe { *(self.buffer.as_ptr() as *const u16) } as usize;
+        let end = 2 + len * sizeof!(PoseOrElse);
+        let slice = &self.buffer[2..end];
+        self.buffer = &self.buffer[end..];
+        PoseInputStream {
+            state: Default::default(),
+            buffer: slice,
+        }
+    }
+
+    /// 从流前分割一个话题配置
     fn slice_config(&mut self) -> &'a Config {
         let config = (&self.buffer).as_ptr() as *const Config;
-        self.buffer = &self.buffer[std::mem::size_of::<Config>()..];
-        unsafe { config.as_ref().unwrap() }
+        self.buffer = &self.buffer[sizeof!(Config)..];
+        unsafe { config.as_ref() }.unwrap()
     }
 }
 
@@ -56,7 +89,7 @@ impl<'a> Iterator for FrameInputStream<'a> {
         } else {
             let topic = self.slice_str();
             let config = self.slice_config();
-            let stream = PoseInputStream(&self.buffer);
+            let stream = self.slice_poses();
             Some(Self::Item {
                 title: self.title,
                 topic,
@@ -70,7 +103,40 @@ impl<'a> Iterator for FrameInputStream<'a> {
     }
 }
 
+impl<'a> Iterator for PoseInputStream<'a> {
+    type Item = (Pose, Color);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while !self.buffer.is_empty() {
+            let pose_or_else = unsafe { *(self.buffer.as_ptr() as *const PoseOrElse) };
+            self.buffer = &self.buffer[sizeof!(PoseOrElse)..];
+            if pose_or_else.is_pose() {
+                return Some((pose_or_else.pose(), self.state));
+            } else {
+                self.state = pose_or_else.color()
+            }
+        }
+        None
+    }
+}
+
+impl PoseOrElse {
+    fn is_pose(&self) -> bool {
+        !unsafe { self.pose }.x.is_nan()
+    }
+
+    fn pose(&self) -> Pose {
+        unsafe { self.pose }
+    }
+
+    fn color(&self) -> Color {
+        let RGBA(r, g, b, a) = unsafe { self.color }.1;
+        Color::from_rgba8(r, g, b, a as f32 / 255.0)
+    }
+}
+
 #[test]
 fn verify_size() {
-    assert_eq!(std::mem::size_of::<Config>(), 16);
+    assert_eq!(sizeof!(Config), 16);
+    assert_eq!(sizeof!(PoseOrElse), sizeof!(Pose));
 }
